48bc2f29554aee22380a07d321c7f65f45975eea
gerni17 <gerni@student.ethz.ch>
2020-12-09 17:57:53 +0100

    snaga

--- flightlib/src/bridges/unity_bridge.cpp
+++ flightlib/src/bridges/unity_bridge.cpp
@@ -5,8 +5,8 @@ namespace flightlib {
 // constructor
 UnityBridge::UnityBridge()
   : client_address_("tcp://*"),
-    pub_port_("10253"),
-    sub_port_("10254"),
+    pub_port_("10270"),
+    sub_port_("10271"),
     num_frames_(0),
     last_downloaded_utime_(0),
     last_download_debug_utime_(0),
